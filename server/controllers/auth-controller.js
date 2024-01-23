54a1edb33c8465b6b1f71fcc125ca28e74207910
hiteshcreator <thehiteshcreator@gmail.com>
2024-01-23 23:34:37 +0530

    dashboard page

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -15,4 +15,12 @@ const register = async (req,res) => {
     }
 }
 
-module.exports = { home,register};
+const dashboard = async (req,res) => {
+    try{
+        res.status(200).send("welcome to dashboard");
+    }catch (error){
+       res.status(400).send("Page is not found!!!");
+    }
+}
+
+module.exports = { home,register,dashboard};
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -5,5 +5,7 @@ const authcontrollers = require("../controllers/auth-controller");
 
 router.route("/").get(authcontrollers.home);
 router.route("/register").get(authcontrollers.register);
+router.route("/dashboard").get(authcontrollers.dashboard);
+
 
 module.exports = router;
