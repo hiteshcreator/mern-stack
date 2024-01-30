9d5d5ec8c60f610d85fbd46571b1808809cbcb62
hiteshcreator <thehiteshcreator@gmail.com>
2024-01-30 23:40:24 +0530

    mongodb setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
 const bodyParser = require('body-parser');
+const connectDb  = require("./utils/db");
 
 // this is use for sending response in API to json format
 // app.use(express.json());
-    
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -16,7 +18,9 @@ app.use("/api/auth",router);
 
 const PORT = 5000;
 
-app.listen(PORT, (err) => {
-    if (err) console.log("->>",err);
-    console.log(`server stared at this Port ${PORT}`);
-});
+connectDb().then( () => {
+    app.listen(PORT, (err) => {
+        if (err) console.log("->>",err);
+        console.log(`server stared at this Port ${PORT}`);
+    });
+})
