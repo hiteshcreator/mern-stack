8a15b29a8dbce3c4cced4ce86e2e7751ce4bea7d
hiteshcreator <thehiteshcreator@gmail.com>
2024-03-19 11:13:52 -0700

    Frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
+import { About } from './pages/About';
+import { Service } from './pages/service';
+// import Navbar from './components/Navbar';
+
 const App = () =>{
-  return <>
-    <h1> our main project</h1>
-  </>
+  return (
+    <>
+      <BrowserRouter>
+       {/* <Navbar /> */}
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/service" element={<Service/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
